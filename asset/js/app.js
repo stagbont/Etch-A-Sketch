2b577689f8e591d3471d8343e5d90735b17bc6a1
Bob Stanley Agbontor <stanleyagbontor63@gmail.com>
2022-07-21 19:30:09 +0000

    Done with the following functions     Generate square     Generate Grid     Set Default Background     Set color for the change of the Background color     Color Picker

--- asset/js/app.js
+++ asset/js/app.js
@@ -19,3 +19,87 @@ if(closeBtn) {
         settingContainer.classList.remove("show__sett");
     })
 }
+
+
+
+
+//
+const mainContainer = document.getElementById("main-container");
+const selectValue = document.getElementById("select__value");
+const selectedValue = document.getElementById("selected-value");
+
+// assign default range value to the selected value
+selectedValue.textContent = selectValue.value;
+var value = selectValue.value;
+generateGrid(value, value);
+generateSquares(value);
+setDefaultBackground();
+listenForAClickAndMove()
+colorPicker();
+
+// generate the grid using the rows and columns
+function generateGrid (rows, columns) {
+    // set grid template for main container
+    mainContainer.style.setProperty('grid-Template-Rows', 'repeat(' + rows + ', 1fr)');
+    mainContainer.style.setProperty('grid-Template-columns', 'repeat(' + columns + ', 1fr)');
+}
+
+// generate the squares
+function generateSquares(number) {
+    for (let i = 0; i < number * number; i++) {
+        // create div for the square
+        const div = document.createElement("div");
+        // set class attribute to square
+        div.setAttribute("class", "square");
+        // Append the div to the main container
+        mainContainer.appendChild(div);
+    }
+}
+
+// set default square background color
+function setDefaultBackground() {
+    const squares = [...document.querySelectorAll(".square")];
+    // get the individual squares
+    for (const square of squares) {
+        // Assign to all a background color
+        square.style.background = "#D9D9D9";
+    }
+}
+
+// listen for theses events click and drag
+function listenForAClickAndMove() {
+    // get the objects of all the squares generated
+    const squares = [...document.querySelectorAll(".square")];
+
+    // loop through each of them
+    for (const square of squares) {
+        // listen for a mouse move over the each square
+        square.addEventListener('mouseover', changeBackgroundColor);
+    }
+}
+
+// Change Background Color
+function changeBackgroundColor() {
+    this.style.background = setColor();
+}
+
+// set color to the color picker 
+function setColor() {
+    // Get the input color element
+    var colorPicker = document.getElementById('colorPicker');
+    return colorPicker.value;
+}
+
+// color picked
+function colorPicker() {
+    // get the input color element
+    var colorPicker = document.getElementById('colorPicker');
+    // listen for change and mousemove
+    colorPicker.addEventListener('change', colorUpdate);
+    colorPicker.addEventListener('mousemove', colorUpdate);
+}
+
+// color update function
+function colorUpdate() {
+    this.setAttribute('value', this.value);
+}
